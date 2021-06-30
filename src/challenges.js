f77c2767df53ce469e5371d69900982fed64e446
Thales Lima <thales.bplima@gmail.com>
2021-06-29 21:05:51 -0300

    add ex até 8 commitão rocket

--- src/challenges.js
+++ src/challenges.js
@@ -21,23 +21,49 @@ function concatName(arrayStrings) {
 }
 
 // Desafio 5
-function footballPoints() {
-  
+function footballPoints(wins, ties) {
+  return wins * 3 + ties;
 }
 
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(arrayNumeros) {
+  let maiorNumero = Math.max.apply(null, arrayNumeros);
+  let contMaiorNumero = 0;
+  for (index = 0; index < arrayNumeros.length; index += 1) {
+    if (arrayNumeros[index] === maiorNumero) {
+      contMaiorNumero += 1;
+    }
+  }
+  return contMaiorNumero;
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distCat1 = Math.abs(mouse - cat1);
+  let distCat2 = Math.abs(mouse - cat2);
+  if (distCat1 < distCat2) {
+    return 'cat1';
+  } if (distCat2 < distCat1) {
+    return 'cat2';
+  } if (distCat1 === distCat2) {
+    return 'os gatos trombam e o rato foge';
+  }
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(stringNumeros) {
+  let novoArray = [];
+  for (let number of stringNumeros)
+    if (number % 3 === 0 && number % 5 !== 0) {
+      novoArray.push('fizz');
+    } else if (number % 5 === 0 && number % 3 !== 0) {
+      novoArray.push('buzz');
+    } else if (number % 3 === 0 && number % 5 === 0) {
+      novoArray.push('fizzBuzz');
+    } else if (number % 3 !== 0 && number % 5 !== 0) {
+      novoArray.push('bug!');
+    }
+  return(novoArray);
 }
 
 // Desafio 9
