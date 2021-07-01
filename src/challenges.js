e7621ae95ae758550f8f1e58e1bce9b66fd01e49
Thales Lima <thales.bplima@gmail.com>
2021-07-01 18:45:36 -0300

    refatorados exs6-8 e add ex9

--- src/challenges.js
+++ src/challenges.js
@@ -5,50 +5,60 @@ function compareTrue(bool1, bool2) {
   }
 }
 
+
 // Desafio 2
 function calcArea(base, altura) {
   return (base * altura) / 2;
 }
+
 
 // Desafio 3
 function splitSentence(string) {
   return string.split(' ');
 }
 
+
 // Desafio 4
 function concatName(arrayStrings) {
   return `${arrayStrings[arrayStrings.length - 1]}, ${arrayStrings[0]}`;
 }
+
 
 // Desafio 5
 function footballPoints(wins, ties) {
   return wins * 3 + ties;
 }
 
+
 // Desafio 6
 function highestCount(arrayNumeros) {
-  let maiorNumero = Math.max.apply(null, arrayNumeros);
-  let contMaiorNumero = 0;
-  for (index = 0; index < arrayNumeros.length; index += 1) {
-    if (arrayNumeros[index] === maiorNumero) {
-      contMaiorNumero += 1;
-    }
-  }
-  return contMaiorNumero;
+  ordemCrescente(arrayNumeros);
+  return counter(arrayNumeros, arrayNumeros[arrayNumeros.length - 1]);
 }
 
-/* // Desafio 7
-function catAndMouse(mouse, cat1, cat2) {
-  let distCat1 = Math.abs(mouse - cat1);
-  let distCat2 = Math.abs(mouse - cat2);
-  if (distCat1 < distCat2) {
-    return 'cat1';
-  } if (distCat2 < distCat1) {
-    return 'cat2';
-  } if (distCat1 === distCat2) {
-    return 'os gatos trombam e o rato foge';
+function ordemCrescente(numbers) {
+  for (let index = 1; index < numbers.length; index += 1) {
+    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+        if (numbers[index] < numbers[secondIndex]) {
+            let aux = numbers[index];
+            numbers[index] = numbers[secondIndex];
+            numbers[secondIndex] = aux;            
+        }
+    }
   }
-} */
+  return(numbers); // retorna o mesmo Array, já organizado em ordem crescente
+}
+
+function counter(arrayNumbers, number) {
+  let count = 0
+  for (let index in arrayNumbers) {
+    if (arrayNumbers[index] === number) {
+      count += 1;
+    }
+  }
+  return (count); // retorna a quantidade de vezes que (number) foi observada no array (arrayNumbers)
+}
+
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
@@ -81,96 +91,74 @@ function mouseHunt (dist1, dist2) {
   }
 }
 
+
 // Desafio 8
-/* function fizzBuzz(arrayNumeros) {
-  let novoArray = [];
-  for (let number of arrayNumeros)
-    if (number % 3 === 0 && number % 5 !== 0) {
-      novoArray.push('fizz');
-    } else if (number % 5 === 0 && number % 3 !== 0) {
-      novoArray.push('buzz');
-    } else if (number % 3 === 0 && number % 5 === 0) {
-      novoArray.push('fizzBuzz');
-    } else if (number % 3 !== 0 && number % 5 !== 0) {
-      novoArray.push('bug!');
-    }
-  return(novoArray);
-} */
-
-function fizzBuzz(arrayN) {
-  fizz(arrayN);
-  buzz(arrayN);
-  fizzbuzz(arrayN);
-  bug(arrayN);
-  return(arrayN); 
+function fizzBuzz(arrayNumeros) {
+  fizzbuzz(arrayNumeros);
+  return arrayNumeros; 
 }
 
-function fizz(array2) {
-  for (let index in array2) {
-    if ((array2[index] % 3 === 0) && (array2[index] % 5 !== 0)) {
-      return array2[index] = 'fizz';
-    }
-  }  
-}
-function buzz(array2) {
-  for (let index in array2) {
-    if ((array2[index] % 5 === 0) && (array2[index] % 3 !== 0)) {
-      return array2[index] = 'buzz';
-    }
-  }
-}
 function fizzbuzz(array2) {
   for (let index in array2) {
-    if ((array2[index] % 3 === 0) && (array2[index] % 5 === 0)) {
-      return array2[index] = 'fizzBuzz';
-    }
+      if ((array2[index] % 3 === 0) && (array2[index] % 5 !== 0)) {
+          array2[index] = 'fizz';
+      } else if ((array2[index] % 5 === 0) && (array2[index] % 3 !== 0)) {
+          array2[index] = 'buzz';
+      } else if ((array2[index] % 3 === 0) && (array2[index] % 5 === 0)) {
+          array2[index] = 'fizzBuzz';
+      } else {  
+          array2[index] = 'bug!';
+      }
   }
+  return array2;
 }
-function bug(array2) {
-  for (let index in array2) {
-    if ((array2[index] % 5 !== 0) && (array2[index] % 3 !== 0)) {
-      return array2[index] = 'bug!';
-    }
-  }
-}
-
-
 
 
 // Desafio 9
 function encode(string) {
-  for(let index = 0; index < string.length; index += 1) {
-    if (string[index] === 'a') {
-      string[index] = 1;
-    } else if (string[index] === 'e') {
-      string[index] = 2;
-    } else if (string[index] === 'i') {
-      string[index] = 3;
-    } else if (string[index] === 'o') {
-      string[index] = 4;
-    } else if (string[index] === 'u') {
-      string[index] = 5;
+  return encode2(string);
+}
+function encode2(string) {   
+  let novaString = ''
+    for(let index of string) {
+      if (index == 'a') {
+        novaString += 1;
+      } else if (index == 'e') {
+        novaString += 2;
+      } else if (index == 'i') {
+        novaString += 3;
+      } else if (index == 'o') {
+        novaString += 4;
+      } else if (index == 'u') {
+        novaString += 5;
+      } else {
+        novaString += index
+      }
     }
-  }
-  return string;
+  return novaString;
 }
 
-
 function decode(string) {
-  for(let numero of string) {
-    if (numero === 1) {
-      numero = 'a';
-    } else if (numero === 2) {
-      numero = 'e';
-    } else if (numero === 3) {
-      numero = 'i';
-    } else if (numero === 4) {
-      numero = 'o';
-    } else if (numero === 5) {
-      numero = 'u';
+  return decode2(string);
+}
+function decode2(string) {              
+  let novaString = ''
+  for(let index of string) {
+    if (index == 1) {
+      novaString += 'a';
+    } else if (index == 2) {
+      novaString += 'e';
+    } else if (index == 3) {
+      novaString += 'i';
+    } else if (index == 4) {
+      novaString += 'o';
+    } else if (index == 5) {
+      novaString += 'u';
+    } else {
+      novaString += index
     }
   }
-  return string;
+  return novaString;
 }
 
 module.exports = {
