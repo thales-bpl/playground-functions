406e893e382aaa657d1302a3c38d637b995c6b32
Thales Lima <thales.bplima@gmail.com>
2021-07-06 02:46:35 -0300

    refatoraçao geral, corrigido desafio3

--- src/challenges.js
+++ src/challenges.js
@@ -5,44 +5,39 @@ function compareTrue(bool1, bool2) {
   }
 }
 
-
 // Desafio 2
 function calcArea(base, altura) {
   return (base * altura) / 2;
 }
 
-
 // Desafio 3
-// Retornando Array vazio. Corrigir.
 function splitSentence(string) {
   return stringSplitter(string);
 }
 
-function stringSplitter(string){
+function stringSplitter(string) {
   let novoArray = [];
   let stringsSeparadas = '';
-  for (let index in string) {
-    } if (string[index] === ' ') {
+  for (let index = 0; index <= string.length; index += 1) {
+    if (string[index] === ' ' || index === string.length) {
       novoArray.push(stringsSeparadas);
       stringsSeparadas = '';
     } else {
-        stringsSeparadas += string[index];
+      stringsSeparadas += string[index];
     }
+  }
   return novoArray;
 }
-
 
 // Desafio 4
 function concatName(arrayStrings) {
   return `${arrayStrings[arrayStrings.length - 1]}, ${arrayStrings[0]}`;
 }
 
-
 // Desafio 5
 function footballPoints(wins, ties) {
   return wins * 3 + ties;
 }
-
 
 // Desafio 6
 function highestCount(arrayNumeros) {
@@ -50,38 +45,39 @@ function highestCount(arrayNumeros) {
   return counter(arrayNumeros, arrayNumeros[arrayNumeros.length - 1]);
 }
 
+/* Retorna o mesmo Array, organizado em ordem crescente usando Bubble Sort */
 function ordemCrescente(numbers) {
   for (let index = 1; index < numbers.length; index += 1) {
     for (let index2 = 0; index2 < index; index2 += 1) {
       if (numbers[index] < numbers[index2]) {
         let aux = numbers[index];
         numbers[index] = numbers[index2];
-        numbers[index2] = aux;            
+        numbers[index2] = aux;
       }
     }
   }
-  return(numbers); // retorna o mesmo Array, organizado em ordem crescente usando Bubble Sort
+  return (numbers);
 }
 
+/* Retorna a quantidade de vezes que (number) foi observada no array (arrayNumbers) */
 function counter(arrayNumbers, number) {
-  let count = 0
+  let count = 0;
   for (let index in arrayNumbers) {
     if (arrayNumbers[index] === number) {
       count += 1;
     }
   }
-  return (count); // retorna a quantidade de vezes que (number) foi observada no array (arrayNumbers)
+  return (count);
 }
-
 
 // Desafio 7
 function catAndMouse(mouse, cat1, cat2) {
   let distCat1 = modulo(mouse - cat1);
   let distCat2 = modulo(mouse - cat2);
-  return mouseHunt (distCat1, distCat2);
+  return mouseHunt(distCat1, distCat2);
 }
 
-// Módulo matemático
+/* Módulo matemático */
 function modulo(number) {
   if (number < 0) {
     return number * (-1);
@@ -90,8 +86,8 @@ function modulo(number) {
   }
 }
 
-// Compara as distâncias entre os gatos e o alvo
-function mouseHunt (dist1, dist2) {
+/* Compara as distâncias entre os gatos e o alvo */
+function mouseHunt(dist1, dist2) {
   if (dist1 < dist2) {
     return 'cat1';
   } if (dist2 < dist1) {
@@ -101,82 +97,81 @@ function mouseHunt (dist1, dist2) {
   }
 }
 
-
 // Desafio 8
-function fizzBuzz(arrayNumeros) {
-  fizzbuzz(arrayNumeros);
-  return arrayNumeros; 
-}
-
-function fizzbuzz(array2) {
-  for (let index in array2) {
-    if ((array2[index] % 3 === 0) && (array2[index] % 5 !== 0)) {
-      array2[index] = 'fizz';
-    } else if ((array2[index] % 5 === 0) && (array2[index] % 3 !== 0)) {
-      array2[index] = 'buzz';
-    } else if ((array2[index] % 3 === 0) && (array2[index] % 5 === 0)) {
-      array2[index] = 'fizzBuzz';
-    } else {  
-      array2[index] = 'bug!'; 
-    }
+function fizzBuzz(arrayNumbers) {
+  let arrayFizzBuzz = [];
+  for(let index of arrayNumbers) {
+    fizz(index, arrayFizzBuzz);
+    buzz(index, arrayFizzBuzz);
+    fizzbuzz(index, arrayFizzBuzz);
+    bug(index, arrayFizzBuzz);
   }
-  return array2;
+  return arrayFizzBuzz;
 }
-// Not optimal. Ideia inicial era criar 1 função pra cada valor a ser substituído mas function bug(arrayNumeros) não estava transformando valores numericos em 'bug!'. if (array2[index] === Number) como critério não adiantou então resolvi passar um else = bug!. Pra fazer isso, tive que jogar tudo na mesma function (ou o 'else' iria considerar strings como válidos e transformar tudo em 'bug!')
 
+function fizz(numero, array) {
+  if (numero % 3 === 0 && numero % 5 !== 0) {
+    array.push('fizz');
+  }
+}
+
+function buzz(numero, array) {
+  if ((numero % 5 === 0) && (numero % 3 !== 0)) {
+    array.push('buzz');
+  }
+}
+
+function fizzbuzz(numero, array) {
+  if ((numero % 3 === 0) && (numero % 5 === 0)) {
+    array.push('fizzBuzz');
+  }
+}
+
+function bug(numero, array) {
+  if ((numero % 5 !== 0) && (numero % 3 !== 0)) {
+    array.push('bug!');
+  }
+}
 
 // Desafio 9
-function encode(string) {  
-  let encoded = '';
-  for(let index of string) {
-    encoded += encode2(index);
+function encode(string) {
+  let encodedString = '';
+  for (let index of string) {
+    encodedString += encoded(index);
   }
-  return encoded;
+  return encodedString;
 }
 
-function encode2(caractere) { 
-  let novaString = ''  
-  if (caractere == 'a') {
-    novaString += 1;
-  } else if (caractere == 'e') {
-    novaString += 2;
-  } else if (caractere == 'i') {
-    novaString += 3;
-  } else if (caractere == 'o') {
-    novaString += 4;
-  } else if (caractere == 'u') {
-    novaString += 5;
-  } else {
-    novaString += caractere
+/* For Loop que compara 1 caractere (index da string alvo) com elementos do arrayEncode. Retorna número se check for verdadeiro */
+function encoded(caractere) {
+  let novaString = caractere;
+  let arrayEncode = ['a', 'e', 'i', 'o', 'u'];
+  for (let index = 1; index < 6; index += 1) {
+    if (caractere === arrayEncode[index - 1]) {
+      novaString = index;
+    }
   }
-  return novaString; // Retorna nova string encodada
+  return novaString;
 }
-
 
 function decode(string) {  
-  let decoded = '';
-  for(let index of string) {
-    decoded += decode2(index);
+  let decodedString = '';
+  for (let index of string) {
+    decodedString += decoded(index);
   }
-  return decoded;
+  return decodedString;
 }
 
-function decode2(caractere) {              
-  let novaString = '';
-  if (caractere == 1) {
-    novaString += 'a';
-  } else if (caractere == 2) {
-    novaString += 'e';
-  } else if (caractere == 3) {
-    novaString += 'i';
-  } else if (caractere == 4) {
-    novaString += 'o';
-  } else if (caractere == 5) {
-    novaString += 'u';
-  } else {
-    novaString += caractere
+/* For Loop que compara 1 caractere (index da string alvo) com elementos do arrayDecode. Retorna vogal se check for verdadeiro */
+function decoded(caractere) {
+  let novaString = caractere;
+  let arrayDecode = ['a', 'e', 'i', 'o', 'u'];
+  for (let index = 1; index < 6; index += 1) {
+    if (caractere == index) {
+      novaString = arrayDecode[index - 1];
+    }
   }
-  return novaString; // Retorna nova string decodada
+  return novaString;
 }
 
 module.exports = {
--- src/challenges2.js
+++ src/challenges2.js
@@ -4,11 +4,11 @@ function techList(tech, name) {
   return listaObjetos(tech, name);
 }
 
-// input: array de strings
-// output: o mesmo array de strings em ordem alfabética
+/* input: array de strings
+output: o mesmo array de strings em ordem alfabética (Bubble Sort) */
 function ordemAlfabetica(arrayPalavras) {
   for (let index = 1; index < arrayPalavras.length; index += 1) {
-    for (let index2 = 0; index2 < arrayPalavras.length; index2 += 1) {  
+    for (let index2 = 0; index2 < arrayPalavras.length; index2 += 1) {
       if (arrayPalavras[index].toLowerCase() < arrayPalavras[index2].toLowerCase()) {
         let ordenador = arrayPalavras[index];
         arrayPalavras[index] = arrayPalavras[index2];
@@ -16,11 +16,11 @@ function ordemAlfabetica(arrayPalavras) {
       }
     }
   }
-return arrayPalavras;
+  return arrayPalavras;
 }
 
-// input: (Array de strings, string)
-// output: Novo array de objetos compostos por {string[index], string} 
+/* input: (Array de strings, string)
+output: Novo array de objetos compostos por {string[index], string} */
 function listaObjetos (techs, teacher) {
   if (techs.length === 0) {
     return 'Vazio!';
@@ -37,62 +37,52 @@ function listaObjetos (techs, teacher) {
   }
 }
 
-
 // Desafio 11
 function generatePhoneNumber(arrayDigitos) {
-  validador(arrayDigitos);
-  if (validador(arrayDigitos) === 'Array com tamanho incorreto.' || validador(arrayDigitos) === 'não é possível gerar um número de telefone com esses valores') {
-    return validador(arrayDigitos); 
+  let status = validador(arrayDigitos);
+  if (arrayDigitos.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  } else if (status === 'não é possível gerar um número de telefone com esses valores') {
+    return status; 
   } else {
     return telephoneNumber(arrayDigitos);
   }
 }
 
-// Função não está funcionando adequadamente. Nunca retorna ('não é possível gerar um número de telefone com esses valores'). Não está reconhecendo condição mesmo quando função numberCounter > 2 quando colocamos um }else{ ao fim da função.
-
-// Função que verifica se diretrizes para número de telefone foram cumpridas
+/* Função que verifica se diretrizes para número de telefone foram cumpridas */
 function validador(arrayAlgarismos) {
-  if (arrayAlgarismos.length !== 11) {
-    return 'Array com tamanho incorreto.';
-  } else {
-    for (let index in arrayAlgarismos) {
-      if (arrayAlgarismos[index] < 0 || arrayAlgarismos[index] > 9 || numberCounter(arrayAlgarismos, arrayAlgarismos[index]) > 2) {
-        return 'não é possível gerar um número de telefone com esses valores';
-      }
+  for (let value of arrayAlgarismos) {
+    if (value < 0 || value > 9 || numberCounter(arrayAlgarismos, arrayAlgarismos[value]) > 2) {
+      return 'não é possível gerar um número de telefone com esses valores';
     }
   }
 }
 
-
-// Função que conta frequência de um dígito dentro de um array
+/* Função que conta frequência de um dígito dentro de um array */
 function numberCounter(arrayNumbers, number) {
   let counter = 0;
-  for (let index in arrayNumbers) {
-    if (arrayNumbers[index] === number) {
+  for (let value of arrayNumbers) {
+    if (value === number) {
       counter += 1;
     }
   }
   return counter;
-} 
+}  
 
-// Função geradora do número de telefone
-function telephoneNumber (arrayDigitos) {
-  let numeroTelefone = [];
-  numeroTelefone += '(';
-  for (let index = 0; index < 2; index += 1) {
+/* Função geradora do número de telefone */
+/* Bug aleatório: se usarmos for/in na L78, (apenas) um dos casos não passa porque ele não adiciona ') ' nem '-'. O index só é reconhecido como 2 e 7 se usarmos o operador '==' ao inves de '==='. Nos outros 4 casos, porém, ele adiciona as strings corretamente. Desse modo, preferimos usar for loop clássico */
+function telephoneNumber(arrayDigitos) {
+  let numeroTelefone = '(';
+  for (let index = 0; index < 11; index += 1) {
+    if (index === 2) {
+      numeroTelefone += ') ';
+    } if (index === 7) {
+      numeroTelefone += '-';
+    }
     numeroTelefone += arrayDigitos[index];
-  }
-  numeroTelefone += ') ';
-  for (let index2 = 2; index2 < 7; index2 += 1) {
-    numeroTelefone += arrayDigitos[index2];
-  }
-  numeroTelefone += '-';
-  for (let index3 = 7; index3 < 11; index3 += 1) {
-    numeroTelefone += arrayDigitos[index3];
   }
   return numeroTelefone;
 }
-
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
@@ -100,36 +90,32 @@ function triangleCheck(lineA, lineB, lineC) {
   let checkB = sideCheck(lineB, lineC, lineA);
   let checkC = sideCheck(lineC, lineA, lineB);
   if (checkA === true && checkB === true && checkC === true) {
-      return true;
+    return true;
   } else {
-      return false;
+    return false;
   }
 }
 
-// Checa se um lado é menor que a soma dos outros dois E maior que a diferença dos módulos dos outros dois lados
+/* Checa se um lado é menor que a soma dos outros dois E maior que a diferença dos módulos dos outros dois lados */
 function sideCheck (line1, line2, line3) {
-    if (line1 < line2 + line3 && line1 > modulo(line2 - line3)) {
-        return true;
-    } else {
-        return false;
-    }    
+  if (line1 < line2 + line3 && line1 > modulo(line2 - line3)) {
+    return true;
+  }
 }
 
-// Módulo matemático
+/* Módulo matemático */
 function modulo(number) {
   if (number < 0) {
-      return number * (-1)
-  } else if (number >= 0) {
-      return number
+    return number * (-1);
+  } if (number >= 0) {
+    return number;
   }
 }
-
 
 // Desafio 13
 function hydrate() {
   // seu código aqui
 }
-
 
 module.exports = {
   generatePhoneNumber,
